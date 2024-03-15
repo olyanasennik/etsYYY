627982a59a4fada0608f9b07357a67dde9fb5eb8
olyanasennik <nasennikolya@gmail.com>
2024-03-14 20:49:26 -0400

    Initial commit1

---
+++ src/test/etsY/src/test/java/Tests/TestBase.java
@@ -0,0 +1,27 @@
+package Tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utilities.Driver;
+
+import java.time.Duration;
+
+/**
+ * This class contains common configuration methods for all other test classes in this framewor
+ * All Test classes should extend this class to obtain the common configurations
+ */
+public class TestBase {
+
+    @BeforeMethod (alwaysRun = true)
+    public void setup(){
+        Driver.getDriver().manage().window().maximize();
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+    }
+
+    @AfterMethod (alwaysRun = true)
+    public void tearDown(){
+
+        Driver.quitDriver();
+    }
+}
